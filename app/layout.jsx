8ab4744b15a845ee244fcc47523c41f77d19b3fe
angelberger09 <153780755+angelberger09@users.noreply.github.com>
2026-06-25 03:51:56 -0400

    Load Portfolio paper archive pass

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import './paper-header-pass.css';
 import './header-paper-tabs-pass.css';
 import './root-paper-pass.css';
 import './about-paper-collage-pass.css';
+import './portfolio-paper-archive-pass.css';
 import './store-paper-snippet-pass.css';
 import './store-preview-collage-pass.css';
 import './store-hierarchy-pass.css';

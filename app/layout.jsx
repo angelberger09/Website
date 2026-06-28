654efb17b6f741710a8c12828296fc9703e0da31
angelberger09 <153780755+angelberger09@users.noreply.github.com>
2026-06-28 16:33:44 -0400

    Load root paper budget pass

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import './paper-header-pass.css';
 import './header-paper-tabs-pass.css';
 import './header-active-room-pass.css';
 import './root-paper-pass.css';
+import './root-paper-budget-pass.css';
 import './home-paper-collage-pass.css';
 import './home-highlight-photo-pass.css';
 import './home-front-room-welcome-desk-pass.css';
